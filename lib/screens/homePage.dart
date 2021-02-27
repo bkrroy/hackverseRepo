e0d98407c063b384d3eb9884f5e7f0feda1043e8
Dhruv Singh <dhruv0607singh@gmail.com>
2021-02-27 23:05:10 +0530

    modified homePage

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -27,73 +27,92 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: Color(0xFF253A4B),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             SizedBox(
               height: 100.0,
             ),
             Expanded(
+              flex: 7,
               child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: FlatButton(
-                  color: Color(0xFFFF5252),
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
                   child: Text(
-                    'Create Game',
+                    'Welcome User!',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 50.0,
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, GamePage.id);
-                  }, //Navigator.pushNamed(context, GamePage.id);
                 ),
               ),
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: FlatButton(
-                  color: Color(0xFFFF5252),
-                  child: Text(
-                    'Join Game',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 50.0,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: FlatButton(
+                      color: Color(0xFFFF5252),
+                      child: Text(
+                        'Create Game',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, GamePage.id);
+                      }, //Navigator.pushNamed(context, GamePage.id);
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, GameJoin.id);
-                  },
                 ),
-              ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: FlatButton(
+                      color: Color(0xFFFF5252),
+                      child: Text(
+                        'Join Game',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, GameJoin.id);
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50.0,
             ),
             Padding(
               padding: const EdgeInsets.all(24.0),
               child: GestureDetector(
                 child: Container(
-                  height: 100,
+                  height: 50,
                   color: Color(0xFFFF5252),
                   child: Center(
                     child: Text(
                       'Sign Out',
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 40,
+                        fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                 ),
-                onTap: (){
-                  Provider.of<AuthenticationService>(context, listen: false).signOut();
+                onTap: () {
+                  Provider.of<AuthenticationService>(context, listen: false)
+                      .signOut();
                   Navigator.pushNamed(context, WelcomeScreen.id);
                 },
               ),
-            ),
-            SizedBox(
-              height: 100.0,
             ),
           ],
         ),
